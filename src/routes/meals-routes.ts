be7b7956a993d4fa39cebf6f4351e10ef0da8f49
Matheus Arakaki <arakakimath@gmail.com>
2024-11-05 14:50:28 -0400

    registering cookies to save user session

--- src/routes/meals-routes.ts
+++ src/routes/meals-routes.ts
@@ -1,13 +1,35 @@
 import { FastifyInstance } from 'fastify'
 import dayjs from 'dayjs'
+import z from 'zod'
+import { randomUUID } from 'node:crypto'
 
 import { checkBodyMeals } from '../middlewares/check-body-meals'
 import { database } from '../database/database-tools'
+import { getId } from '../middlewares/check-id-valid'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.get('/', (request, reply) => {
-    return database.showAll('meals')
-  })
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    (request, reply) => {
+      return database.showAll('meals')
+    },
+  )
+
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    (request, reply) => {
+      const id = getId(request, reply)
+
+      return database.showOne('meals', id)
+    },
+  )
 
   app.post(
     '/',
@@ -17,6 +39,17 @@ export async function mealsRoutes(app: FastifyInstance) {
     (request, reply) => {
       const { name, description, date, hour, diet } = request.body
 
+      let { sessionId } = request.cookies
+
+      if (!sessionId) {
+        sessionId = randomUUID()
+
+        reply.cookie('sessionId', sessionId, {
+          path: '/',
+          maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+        })
+      }
+
       database.insert('meals', {
         name,
         description,
@@ -25,9 +58,20 @@ export async function mealsRoutes(app: FastifyInstance) {
           .minute(hour.split(':')[1])
           .format('DD/MM/YY, HH:mm'),
         diet,
+        session_id: sessionId,
       })
 
       return reply.status(201).send()
+    },
+  )
+
+  app.put(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    (request, reply) => {
+      const id = getId(request, reply)
     },
   )
 }
