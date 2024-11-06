25926adc735051ce8a195a7a6ac414fe7f7b98a7
Matheus Arakaki <arakakimath@gmail.com>
2024-11-05 22:43:20 -0400

    sixth attempt

--- knexfile.ts
+++ knexfile.ts
@@ -1,3 +1,18 @@
-import { config } from "./src/database"
+import { Knex } from 'knex'
+
+const config: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
 
 export default config
