6ec232c3e135987c547b8cf92154c956d2ec0be9
Matheus Arakaki <arakakimath@gmail.com>
2024-11-05 22:34:00 -0400

    third attempt

--- src/database/database-setup.ts
+++ src/database/database-setup.ts
@@ -7,7 +7,12 @@ if (!env.DATABASE_URL) {
 }
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: { filename: env.DATABASE_URL },
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
